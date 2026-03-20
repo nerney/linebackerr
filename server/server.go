98b768d103d41ccd065f7f23320fefe9080ddcc8
Jacob Nerney <nerneyj@gmail.com>
2026-03-20 09:56:10 -0400

    refactor(server): switch HTTP server to Echo

--- server/server.go
+++ server/server.go
@@ -1,35 +1,36 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 const defaultAddr = ":6666"
 
 type Server struct {
-	httpServer *http.Server
-}
-
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "OK")
+	e    *echo.Echo
+	addr string
 }
 
 func Init() *Server {
-	mux := http.NewServeMux()
+	e := echo.New()
+
+	// Middleware currently applied globally for all routes.
+	e.Use(middleware.Recover())
 
 	// Routes currently live in this package; add/register new HTTP routes here for now.
-	mux.HandleFunc("/health", healthHandler)
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
 
 	return &Server{
-		httpServer: &http.Server{
-			Addr:    defaultAddr,
-			Handler: mux,
-		},
+		e:    e,
+		addr: defaultAddr,
 	}
 }
 
 func (s *Server) Start() error {
-	return s.httpServer.ListenAndServe()
+	return s.e.Start(s.addr)
 }
